5bf8c7108a69a224831f2806c387da1a00e4dbcc
James Wong <jwong@redhat.com>
2016-07-14 14:25:22 -0400

    index join table storage_profile_storages

--- db/migrate/20160712044409_create_join_table_storage_profile_storage.rb
+++ db/migrate/20160712044409_create_join_table_storage_profile_storage.rb
@@ -3,6 +3,8 @@ class CreateJoinTableStorageProfileStorage < ActiveRecord::Migration[5.0]
     create_table :storage_profile_storages do |t|
       t.bigint  :storage_profile_id
       t.bigint  :storage_id
+      t.index [:storage_id, :storage_profile_id], :name => 'storage_to_profile'
+      t.index [:storage_profile_id, :storage_id], :name => 'profile_to_storage'
     end
   end
 end
