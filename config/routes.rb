01f7de11970b67fb8b1a66f7763d36964c1f07f0
Jozef Zigmund <jzigmund@redhat.com>
2016-09-29 17:24:47 +0200

    Added missing routes for Network Ports
    https://bugzilla.redhat.com/show_bug.cgi?id=1364089

--- config/routes.rb
+++ config/routes.rb
@@ -1521,6 +1521,7 @@ Vmdb::Application.routes.draw do
       :post => %w(
         button
         quick_search
+        listnav_search_selected
         show
         show_list
         tag_edit_form_field_changed
@@ -1528,6 +1529,7 @@ Vmdb::Application.routes.draw do
       ) +
         adv_search_post +
         compare_post +
+        save_post +
         exp_post
     },
 
