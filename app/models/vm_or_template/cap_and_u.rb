6ae45e7799da5985375fd4f0c5dca96e49ca2ec1
James Wong <jwong@redhat.com>
2016-10-21 01:33:56 -0400

    WIP-2 c and u using influxdb

--- app/models/vm_or_template/cap_and_u.rb
+++ app/models/vm_or_template/cap_and_u.rb
@@ -9,10 +9,17 @@ module VmOrTemplate::CapAndU
 
   def fetch_metrics_available
     # [{:id => metric.id, :name => @supported_metrics[metric.type_id], :type => metric.type, :unit => metric.unit}]
+    [{:id => 'cpu_used_delta_summation', :name => 'cpu_used_delta_summation', :type => Float, :unit => ''}]
   end
 
   def collect_live_metrics(metrics, start_time, end_time, interval)
-    tsdb_connection.read("select count(cpu_usage_rate_average) from metrics where resource_name = 'jwong-vc60'")
+    results = tsdb_connection.read(metrics.pluck(:name), start_time, end_time, interval, 'VmOrTemplate', id)
+    if results && results.length == 1  && results[0].try('values')
+      return results[0]['values'].each_with_object({}) do |pt, m|
+        m[pt.delete('time')] = pt
+      end
+    end
+    []
   end
 
   def collect_stats_metric
