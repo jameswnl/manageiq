49394ddc063948850e2c050f81ac468aa2c7546e
James Wong <jwong@redhat.com>
2016-10-23 02:35:56 -0400

    WIP-3 query for influxdb

--- app/models/vm_or_template/cap_and_u.rb
+++ app/models/vm_or_template/cap_and_u.rb
@@ -9,17 +9,24 @@ module VmOrTemplate::CapAndU
 
   def fetch_metrics_available
     # [{:id => metric.id, :name => @supported_metrics[metric.type_id], :type => metric.type, :unit => metric.unit}]
-    [{:id => 'cpu_used_delta_summation', :name => 'cpu_used_delta_summation', :type => Float, :unit => ''}]
+    # [{:id => 'cpu_used_delta_summation', :name => 'cpu_used_delta_summation', :type => Float, :unit => ''}]
   end
 
   def collect_live_metrics(metrics, start_time, end_time, interval)
-    results = tsdb_connection.read(metrics.pluck(:name), start_time, end_time, interval, 'VmOrTemplate', id)
-    if results && results.length == 1  && results[0].try('values')
-      return results[0]['values'].each_with_object({}) do |pt, m|
-        m[pt.delete('time')] = pt
-      end
-    end
-    []
+    # TODO: this is a hack. need to generalize this for cross TSDB and aggregate methods
+    r = metrics.pluck(:name).map{ |name| name.start_with?('max_') ? "max(#{name[4..-1]}) as #{name}" : name }
+    r2 = r.map{ |name| name.start_with?('min_') ? "min(#{name[4..-1]}) as #{name}" : name }
+    r3 = r2.map{ |name| name.start_with?('v_pct_') ? "mean(#{name[6..-1]}) as #{name}" : name }
+    r4 = r3.map{ |name| name.start_with?('v_derived_')  ? "sum(#{name['v_derived_'.length..-1]}) as #{name}" : name }
+    # metric_names = metrics.pluck(:name).map{ |name| "mean(#{name}) as #{name}" }
+    metric_names = r4
+    results = tsdb_connection.read(
+        :metrics    => metric_names,
+        :start_time => start_time,
+        :end_time   => end_time,
+        :bucket_sec => interval,
+        :resource   => self
+    )
   end
 
   def collect_stats_metric
