8b949389b07f637583a662d015c560f8d150498c
James Wong <jwong@redhat.com>
2018-12-19 17:31:54 -0500

    v2v migration as a Job

--- app/models/conversion_host/operations.rb
+++ app/models/conversion_host/operations.rb
@@ -10,12 +10,15 @@ module ConversionHost::Operations
           _log.error(error)
           # notify_task_status('conversion', false, "name=#{task.source.name}, id=#{task.source.id}")
           # TODO: raise Automate event to flag statemachine failure?
+        ensure
+          task.save
         end
       end
     end
 
     def assign_to_tasks
       pending_tasks = ServiceTemplateTransformationPlanTask.where(:state => 'active', :conversion_host => nil)
+      return if pending_tasks.empty?
       by_ems = pending_tasks.sort_by(&:created_on).each_with_object({}) do |task, hash|
         hash[task.destination_ems] = hash[task.destination_ems] || []
         hash[task.destination_ems].append(task)
@@ -27,6 +30,7 @@ module ConversionHost::Operations
           eligible_hosts = ems.conversion_hosts.select(&:eligible?).sort_by { |ch| ch.active_tasks.size }
           break if slots <= 0 || eligible_hosts.empty?
           task.conversion_host = eligible_hosts.first
+          task.save
           slots -= 1
         end
       end
